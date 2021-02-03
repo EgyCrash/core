901d87e297e9cfe12df82482ec8301bf5c9127a7
David Christofas <dchristofas@owncloud.com>
2021-02-03 15:21:04 +0100

    pre-signed download urls for password protected public links
    To support clients which don't use cookies I implemented pre-signed urls
    for password protected public links. The share password is used as the
    signing key and the signed url is then added to the propfind response in
    the field `downloadURL` which was added before but never used. This
    change allows owncloud Web to implement a more efficient download
    mechanism for password protected link shares.

--- apps/dav/lib/Files/PublicFiles/PublicFilesPlugin.php
+++ apps/dav/lib/Files/PublicFiles/PublicFilesPlugin.php
@@ -148,6 +148,25 @@ class PublicFilesPlugin extends ServerPlugin {
 				return $node->getNode()->getSize();
 			});
 		}
+		$server = $this->server;
+		$propFind->handle(FilesPlugin::DOWNLOADURL_PROPERTYNAME, static function () use ($node, $server) {
+			$path = $server->getBaseUri() . $server->getRequestUri();
+			$nodeName = $node->getName();
+			if (\substr($path, -\strlen($nodeName)) !== $nodeName) {
+				$path .= '/' . $nodeName;
+			}
+
+			$share = $node->getShare();
+			if ($share->getPassword() === null) {
+				return $path;
+			}
+
+			$validUntil = new \DateTime();
+			$validUntil->add(new \DateInterval("PT30M")); // valid for 30 minutes
+			$key = \hash_hkdf('sha256', $share->getPassword());
+			$s = new PublicShareSigner($share->getToken(), $node->getName(), $validUntil, $key);
+			return $path . '?signature=' . $s->getSignature() . '&expires=' . \urlencode($validUntil->format(\DateTime::ATOM));
+		});
 	}
 
 	/**
